440c9c2db6778b354f520495aa15a92cd1888845
Jonathan Gilbert <dev@trillica.com>
2018-12-23 11:14:01 +1100

    Fixed typo

--- wmp.js
+++ wmp.js
@@ -50,7 +50,7 @@ function parseResponseLine(wmpLine) {
     }
 
     if (rv.type === "SETTINGS")
-        if (rv.feature === "AMBTMP" || rv.feature === "SETPTEMP") {
+        if (rv.feature === "AMBTMP" || rv.feature === "SETPTEMP")
             rv.value = rv.value / 10;
 
     return rv;
